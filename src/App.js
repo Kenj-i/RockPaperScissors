c5cd1279c2be05f7ca3640f24464318faa766f3c
kenj-i <raphael.gisler@stud.edubs.ch>
2022-10-01 20:50:34 +0200

    Created Rock Paper Scissors game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [yourScore, setYourScore] = useState(0)
+    const [enemyScore, setEmenyScore] = useState(0)
+    
+    const handleOption = (yourChoice) => {
+        const enemyChoice = Math.floor(Math.random() * 3)
+        if (yourChoice == enemyChoice) console.log("Tie!")
+        if (yourChoice == 0 && enemyChoice == 1) setYourScore(prevYourScore => prevYourScore + 1)
+        if (yourChoice == 0 && enemyChoice == 2) setEmenyScore(prevEnemeyScore => prevEnemeyScore +1)
+        if (yourChoice == 1 && enemyChoice == 0) setEmenyScore(prevEnemeyScore => prevEnemeyScore +1)
+        if (yourChoice == 1 && enemyChoice == 2) setYourScore(prevYourScore => prevYourScore + 1)
+        if (yourChoice == 2 && enemyChoice == 0) setYourScore(prevYourScore => prevYourScore + 1)
+        if (yourChoice == 2 && enemyChoice == 1) setEmenyScore(prevEnemeyScore => prevEnemeyScore +1)
+    }
+
+    return (
+        <main>
+            <h1>Welcome to Rock Paper Scissors</h1>
+            <div className='wrap'>
+                <section className='wrap-log'>
+                    Log
+                </section>
+                <section className='wrap-option'>
+                    <div className='wrap-options'>
+                      <div onClick={
+                        () => {
+                            handleOption(0)
+                        }} className='option'>
+                        <h1>🪨</h1>
+                      </div>
+                      <div onClick={
+                        () => {
+                            handleOption(1)
+                        }
+                      } className='option'>
+                        <h1>📄</h1>
+                      </div>
+                      <div onClick={
+                        () => {
+                            handleOption(2)
+                        }
+                      } className='option'>
+                        <h1>✂️</h1>
+                      </div>
+                    </div>
+                    <div className='wrap-scores'>
+                      <h3>Your Score: {yourScore}</h3>
+                      <h3>Enemy Score: {enemyScore}</h3>
+                    </div>
+                </section>
+                <section className='wrap-oponent'>
+
+                </section>
+            </div>
+        </main>
+    );
 }
 
 export default App;
